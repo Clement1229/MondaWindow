64099f5d75a5e2829741380630dc0a5901e0657d
Clement Lin <x4383lin@gmail.com>
2018-01-25 13:45:39 -0600

    redirect to homepage

--- src/main/webapp/static/app.js
+++ src/main/webapp/static/app.js
@@ -4,6 +4,7 @@
 var app = angular.module("myHome", ["ngRoute"]);
 
 app.config(function($routeProvider){
+		console.log('abcdefghijklmnopqrstuvwxyz');
 		$routeProvider.when("/",{
 			templateUrl : "static/features/home/home.html",
 			controller : "home"
