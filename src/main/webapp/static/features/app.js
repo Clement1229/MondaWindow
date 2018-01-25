b29d9768baf2abf59f20ced8c0eaf7810d342c51
Clement Lin <x4383lin@gmail.com>
2018-01-25 12:16:26 -0600

    homepage.html app.js

--- src/main/webapp/static/features/app.js
+++ src/main/webapp/static/features/app.js
@@ -1,3 +1,15 @@
 /**
  * 
  */
+var app = angular.module("myHome", ["ngRoute"]);
+
+app.config(function($routeProvider){
+		$routeProvider.when("/",{
+			templateUrl : "static/features/home/home.html",
+			controller : "home"
+		})
+
+
+
+
+});
